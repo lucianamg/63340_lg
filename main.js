c9d7529c0aea0edff98c30652d40c4136a51d88a
lucianamg <luciana.exe@gmail.com>
2024-12-08 00:30:01 +0100

    primeras funciones terminadas

--- main.js
+++ main.js
@@ -1,149 +1,129 @@
-
-
-let PassOk = false;
-let attemptPass = 3;
-//las variables de los attempt se declaran antes del DO
-function GetSpaceName(){
-    let SpaceIdentified = false;
+function getSpaceName(){
+    //las variables de los attempt se declaran antes del DO
+    let spaceIdentified = false;
     let attemptSpace = 3;
-    let SpaceName;
+    let spaceName;
     do {
-        SpaceName = prompt ("ingresa el nombre de tu espacio familiar"+ " ( por ejemplo Casa o Familia )").toLowerCase();
+        spaceName = prompt ("ingresa el nombre de tu espacio familiar"+ " ( por ejemplo Casa o Familia )").toLowerCase();
         console.log("intentos = " + attemptSpace);
-        if (SpaceName === null || SpaceName === ""){
+        if (spaceName === null || spaceName === ""){
             alert("Debes ingresar el nombre de tu espacio familiar");
             console.log("ingresó null la familia");
             break;
         }
-        if ((SpaceName === "admin" || SpaceName === "casa" || SpaceName === "cc") && attemptSpace>=1){
+        if ((spaceName === "admin" || spaceName === "casa" || spaceName === "cc") && attemptSpace>=1){
             console.log("ingresó bien la familia");   //lo dejo pasar si escribe familia o casa
-            SpaceIdentified = true;
-            alert("Hola " + SpaceName);
+            spaceIdentified = true;
+            //alert("Hola " + spaceName); //saco los alert para que seguidamente pida user
         }else{ 
-            alert("No se registró la familia " + SpaceName);
+            alert("No se registró la familia " + spaceName);
             attemptSpace--
             console.log("ingresó mal la familia " + attemptSpace);
 
             if (attemptSpace<1){
                 alert("Has hecho varios intentos fallidos, quieres registrarte o necesitas ayuda con tu espacio familiar?");
+                alert("Actualmente no hay proceso para recuperar tu Espacio, recargá y empezá de nuevo con los valores sugeridos en casa paso. Gracias!");
+                console.log("spaceName " + spaceName + ", spaceIdentified " + spaceIdentified);
                 console.log("ingresó mal la familia 3 veces");
                 break;
             }
         }
-    } while (SpaceIdentified === false);
-    return SpaceName;
+    } while (spaceIdentified === false);
+    return [spaceName, spaceIdentified];// para sacar dos valores de un return
 }
-let SpaceName = GetSpaceName ();
-if(SpaceName === null || SpaceName === ""){
-    console.log("ALERTA Spacename pasó en null ");
+function login (){
+    let returnGetSpaceName = getSpaceName ();
+    let spaceName = returnGetSpaceName[0];
+    let spaceIdentified = returnGetSpaceName[1];
+    if (spaceName === null || spaceName === "" || spaceIdentified !== true){
+        return
+    }
+    console.log("spaceName " + spaceName + ", spaceIdentified " + spaceIdentified);
+    //let userName = getUser();
+//}
+    // PARA PASS
+//function stopLogin2 (){
+    let returnGetUser = getUser ();
+    let user = returnGetUser[0];
+    let userIdentified = returnGetUser[1];
+    if (user === null || user === "" || userIdentified !== true){
+        return
+    }
+    console.log("userName " + user + ", userIdentified " + userIdentified);
+    //let pass = getPass();
+    let returnPass = getPass ();
+    let passw = returnPass[0];
+    let passOk = returnPass[1];
+    if (passw === null || passw === "" || passOk !== true){
+        return
+    }
+    console.log("TENEMOS "+ "Pass " + passw + ", pass ok " + passOk);
 }
-console.log(" La casa es " + SpaceName);
- // aca tendria que revisar que nunca llegue en null hasta este punto
- while(SpaceName === null || SpaceName === ""){
-    break;
-}
-
-function GetUser(){
-    let UserIdentified = false;
+function getUser(){
+    let userIdentified = false;
     let attemptUser = 3;
-    let UserName;
+    let userName;
     do {
-        if(SpaceName === null || SpaceName === ""){
-            break;
-        }
-        UserName = prompt ("ingresa tu usuario"+ " ( por ejemplo User o Admin )");
+        userName = prompt ("ingresa tu usuario"+ " ( por ejemplo User o Admin )");
         console.log("intentos = " + attemptUser);
-        if (UserName === null || UserName === ""){
+        if (userName === null || userName === ""){
             alert("Debes ingresar tu usuario");
             console.log("ingresó null el usuario");
             break;
         }
-        if ((UserName === "User" || UserName === "Admin" || UserName === "aa") && attemptUser>=1){
+        if ((userName === "User" || userName === "Admin" || userName === "aa") && attemptUser>=1){
             console.log("ingresó bien el usuario");  
-            UserIdentified = true;
-            alert("Hola " + UserName);
+            userIdentified = true;
+            //alert("Hola " + userName); //saco los alert para que seguidamente pida pass
         }else{ 
-            alert("No se registró el usuario " + UserName);
+            alert("No se registró el usuario " + userName);
             attemptUser--
-            console.log("ingresó mal el usuario, quedan " + attemptuser + " intentos." );
+            console.log("ingresó mal el usuario, quedan " + attemptUser + " intentos." );
 
             if (attemptUser<1){
                 alert("Has hecho varios intentos fallidos, quieres recuperar tu usuario?");
+                alert("Actualmente no hay proceso para recuperar tu usuario, recargá y empezá de nuevo con los valores sugeridos en casa paso. Gracias!");
+                console.log("userName " + userName + ", userIdentified " + userIdentified);
                 console.log("ingresó mal el usuario 3 veces");
                 break;
             }
         }
-    } while (UserIdentified === false);
-    return UserName;
-} git config --global user.name "lucianamg"
-git config --global user.email luciana.exe@gmail.com
-
-let UserName = GetUser ();
-if(UserName === null || UserName === ""){
-    console.log("ALERTA Username pasó en null ");
+    } while (userIdentified === false);
+    return [userName, userIdentified];
 }
-console.log(" El Username es " + UserName); 
-
-/* 
-function PassOk(){
+function getPass(){
+    let passOk = false;
+    let attemptPass = 3;
+    let pass;
     do {
-        
-    } while (condition);
-} */
+        pass = prompt ("ingresa tu contraseña"+ " ( por ejemplo pass o Dejamentrar )");
+        console.log("intentos = " + attemptPass);
+        if (pass === null || pass === ""){
+            alert("Debes ingresar tu contraseña");
+            console.log("ingresó null la contraseña");
+            break;
+        }
+        if ((pass === "pass" || pass === "Dejamentrar" || pass === "pp") && attemptPass>=1){
+            console.log("ingresó bien la contraseña");  
+            passOk = true;
+            alert("pass OK");//sacar esta cuendo pueda
+        }else{ 
+            alert("La contraseña es incorrecta ");
+            attemptPass--
+            console.log("ingresó mal la contraseña, quedan " + attemptPass + " intentos." );
 
-
-
- /* do{
-    
-            
-
-                
-                let usuario = prompt ("ingresa el nombre de usuario");
-                    if (usuario == null || usuario == ""){
-                        alert("Debes ingresar un nombre de usuario"+ " ( por ejemplo user o admin )");
-                        console.log("ingresó null el usuario");
-                        break;
-                    }
-                    if ((usuario === "admin" || usuario === "aa" || usuario === "user") && attemptUser>=1){
-                        UserIdentified = true; //lo dejo pasar si escribe admin o user
-                        console.log("ingresó bien la familia y el usuario");
-
-                            let pass = prompt ("ingresa tu contraseña");
-
-                            if (pass == null || pass == ""){
-                                alert("Debes ingresar una contraseña"+ " ( por ejemplo pass o pp )");
-                                console.log("ingresó null la pass");
-                                break;
-                            }
-                            if ((pass === "pass" || pass === "dd" || pass === "pp") && attemptPass>=1){
-                                PassOk = true; //lo dejo pasar si escribe pass o...
-                            alert("bienvenido a " + familyName + ", " +usuario);
-                            console.log("ingresó bien la pass, proceso completo");
-                            }else{//este else es el de pass
-                                alert("Nombre de usuario o Contraseña incorrectos, te quedan " + (attemptPass-1) + " intentos");
-                                attemptPass--
-                                console.log("ingresó mal la pass");
-
-                                if (attemptPass<1){
-                                alert("Has hecho tres intentos fallidos, quieres recuperar tu contraseña?");
-                                console.log("ingresó mal la pass 3 veces");
-                                break;
-                                }
-                            }console.log("UNO");
-
-                    }else{//este else es el de usuario
-                        alert("Usuario o pass incorrectos, intenta nuevamente.");
-                        attemptUser--
-                        console.log("ingresó mal el user");
-                        UserIdentified = false;
-                        
-                        if (attemptUser<1){
-                            alert("Has hecho tres intentos fallidos, pide ayuda al Administrador de la cuenta.");
-                            console.log("ingresó mal el user 3 veces");
-                            break;
-                        }console.log("dos");
-
-                    }
-    
+            if (attemptPass<1){
+                alert("Has hecho varios intentos fallidos, quieres recuperar tu contraseña?");
+                alert("Actualmente no hay proceso para recuperar tu contraseña, recargá y empezá de nuevo con los valores sugeridos en casa paso. Gracias!");
+                console.log("Ingresó " + pass + ", pass ok " + passOk);
+                console.log("ingresó mal la contraseña 3 veces");
+                break;
+            }
+        }
+    } while (passOk === false);
+    return [pass, passOk];
 }
-while (PassOk === false);  */
+
+login()
+alert("Bienvenido/a " +  /*user*/ + " de la casa " + /*spaceName*/ + "! ahora vamos a calcular unas propinas, tocá aceptar.");
+
