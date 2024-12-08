76b1cadba6fe4cb7d86a4af570fec64bc05d20dd
lucianamg <luciana.exe@gmail.com>
2024-12-08 19:30:04 +0100

    funciones login y calcular-propinas terminadas

--- main.js
+++ main.js
@@ -114,7 +114,7 @@ function getPass(){
 
             if (attemptPass<1){
                 alert("Has hecho varios intentos fallidos, quieres recuperar tu contraseña?");
-                alert("Actualmente no hay proceso para recuperar tu contraseña, recargá y empezá de nuevo con los valores sugeridos en casa paso. Gracias!");
+                alert("Actualmente no hay proceso para recuperar tu contraseña, recargá y empezá de nuevo con los valores sugeridos en cada paso. Gracias!");
                 console.log("Ingresó " + pass + ", pass ok " + passOk);
                 console.log("ingresó mal la contraseña 3 veces");
                 break;
@@ -129,23 +129,66 @@ function sayHello(){
     let finalUserName = getFinalData[0];
     console.log(finalUserName + finalSpaceName);
     alert("Bienvenido/a " +  finalUserName + " del Espacio ´" + finalSpaceName + "´! Ahora vamos a calcular unas propinas, tocá aceptar.");
+    console.log("bonus track calculo de propinas para cuando completaste el user y pass");
 }
 function calcularPropinas(){
-    let invoiceAmmount = parseFloat(prompt ("ingresa el monto final que te aparece en la cuenta"));
-    //convertir a numero decimal
-    let desiredTip = parseFloat(prompt("ingresa -en números- un porcentaje de propina que quieres dejar, por ejemplo 5, 10, 15 o 20 porciento"));
-    //convertir a numero 
-    let divide = parseInt(prompt("ingresa -en números- cuántas personas son para dividir el pago, por ejemplo 3, 5, 10 etc")); 
-    //convertir a numero entero
-    let totalTip = invoiceAmmount*desiredTip/100;
-    let result = invoiceAmmount + totalTip;
-    let dividedAmmount = result/divide;
-    // hacer casos si pone null o "" en alguno de los tres (Nan)
-    // haceer caso si pone negativo
-    
-    alert("Para el porcentaje elegido, la propina total es de $ " + totalTip + ". Para pagar la factura de $ " + invoiceAmmount + " dividida entre " + divide + " personas, con un porcentaje de propina del " + desiredTip + " %, cada uno debe abonar $ " + dividedAmmount);
+    let input = prompt("ingresa el monto final que te aparece en la cuenta");
+    if (input === null || input.trim() === ""){
+        alert("Debes ingresar un monto y no puede ser menor que cero.");
+        //location.reload();
+        return
+    }
+    let invoiceAmount = parseFloat(input);
+    if (isNaN(invoiceAmount) || invoiceAmount<0){
+        alert("Debes ingresar un monto y no puede ser menor que cero.");
+        return
+    }
+    input = prompt("ingresa -en números- un porcentaje de propina que quieres dejar, por ejemplo 5, 10, 15 o 20 porciento")
+    if (input === null || input.trim() === ""){
+        alert("Debes ingresar un porcentaje de propina y debe ser mayor que cero.");
+        return
+    }
+    let desiredTip = parseFloat(input);
+    if (isNaN(desiredTip) || desiredTip<=0){
+        alert("Debes ingresar un porcentaje de propina y debe ser mayor que cero.");
+        return
+    }
+    input = prompt("ingresa -en números- cuántas personas son para dividir el pago, por ejemplo 3, 5, 10 etc");
+    if (input === null || input.trim() === ""){
+        alert("Debes ingresar un numero de personas a dividir la cuenta y debe ser mayor que cero.");
+        return
+    }
+    let members = parseInt(input)
+    if (isNaN(members) || members<=0){
+        alert("Debes ingresar un numero de personas a dividir la cuenta y debe ser mayor que cero.");
+        return
+    }
+    let totalTip = invoiceAmount*desiredTip/100;
+    let result = invoiceAmount + totalTip;
+    let dividedAmount = result/members;
+    return [invoiceAmount, members, totalTip, desiredTip, dividedAmount, result];
 }
+function aplicarcalcularPropinas(){
+
+    let calculador; 
+    do{
+        calculador = calcularPropinas();
+    }
+    while (calculador === null) 
+    let invoiceAmount = calculador[0] 
+    let members = calculador[1]
+    let totalTip = calculador[2] 
+    let desiredTip = calculador[3] 
+    let dividedAmount = calculador[4]
+    let result = calculador[5]
+    console.log("Monto factura ingresado $ " + invoiceAmount);
+    console.log("Porcentaje propina deseada ingresado "+desiredTip);
+    console.log("Total de propina calculado $ " + totalTip);
+    console.log("Total de factura + propina sin dividir $ " + result);
+    console.log("A dividir entre " + members);
+    console.log("Total para cada persona $ " + dividedAmount );
+    alert("Para el porcentaje elegido, la propina total es de $ " + totalTip + ". Para pagar la factura de $ " + invoiceAmount + " dividida entre " + members + " personas, con un porcentaje de propina del " + desiredTip + " %, cada uno debe abonar $ " + dividedAmount);
+    }
+
 sayHello();
-calcularPropinas();
-
-
+aplicarcalcularPropinas()
