bdf5ac22bb0e0d4bd263c6d19a68a5958b643f08
lucianamg <luciana.exe@gmail.com>
2024-12-23 00:27:21 +0100

    funcion crear task y funcion filtro de estados

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// Organizador Tareas
 function getSpaceName(){
     //las variables de los attempt se declaran antes del DO
     let spaceIdentified = false;
@@ -128,8 +129,8 @@ function sayHello(){
     let finalSpaceName = getFinalData[1];
     let finalUserName = getFinalData[0];
     console.log(finalUserName + finalSpaceName);
-    alert("Bienvenido/a " +  finalUserName + " del Espacio ´" + finalSpaceName + "´! Ahora vamos a calcular unas propinas, tocá aceptar.");
-    console.log("bonus track calculo de propinas para cuando completaste el user y pass");
+    alert("Bienvenido/a " +  finalUserName + " del Espacio ´" + finalSpaceName + "´! tocá aceptar para continuar.");
+    //console.log("bonus track calculo de propinas para cuando completaste el user y pass");
 }
 function calcularPropinas(){
     let input = prompt("ingresa el monto final que te aparece en la cuenta");
@@ -189,5 +190,109 @@ function aplicarcalcularPropinas(){
     alert("Para el porcentaje elegido, la propina total es de $ " + totalTip + ". Para pagar la factura de $ " + invoiceAmount + " dividida entre " + members + " personas, con un porcentaje de propina del " + desiredTip + " %, cada uno debe abonar $ " + dividedAmount);
     }
 
-sayHello();
-aplicarcalcularPropinas()
+//sayHello(); COMENTO ESTO PARA QUE NO TENGAN QUE HACER TODOS ESTOS PRIMEROS PASOS EN LAS PRUEBAS DE LA 2DA PRE-ENTREGA
+
+//aplicarcalcularPropinas() Comento esto porque era para practicar funciones
+
+
+// Un poco la idea: TASKS se pueden crear (admin y autorizados), editar (admin y autorizados), cambiar estado (admin y user designado), borrar (admin y autorizados)
+// Users: cada casa tiene 1 o + admin y users. Admin decide quienes hacen qué. Roles: admin puede todo, users pueden lo que admin diga (crear, editar, cambio estado, borrar, etc) 
+
+// PARA ESTA ENTREGA SUMÉ LA CREACION DE OBJETO TAREAS CON UNA FC CONSTRUCTORA, UN ARRAY DE TAREAS CREADAS, UNA FUNCION DE FILTRO, UNA FUNCION FLECHA.
+
+// Para los siguientes pasos me gustaria poder hacer bajas y modificaciones y que los cambios se guarden de alguna manera. Mas adelante darle forma de formulario a la entrada de datos. 
+let tasksCount = 0;
+function newTask(){ //todo esto deberia ser mas como un formulario
+    let title = prompt("ingresa el titulo de la tarea")
+    if (title === null || title.trim() === "" || title === "" ){
+        alert("Debes ingresar un Título.");
+        //location.reload();
+        return
+    }
+    let description = prompt("ingresa una descripcion de la tarea")
+    if (description === null || description.trim() === "" || description === "" ){
+        alert("Debes ingresar una descripcion.");
+        return
+    }
+    let dateCreated = new Date()
+    let userAssigned = prompt("ingresa el tasker") // en el futuro que pueda elegir los uduarios de entre los que hay inscriptos en la casa
+    if (userAssigned === null || userAssigned.trim() === "" || userAssigned === "" ){
+        alert("Debes ingresar un Tasker.");
+        return
+    }else{//que use la fc filtrar tasker para chequear que esos usaurios existen
+
+    }
+    let status = prompt("ingresa el Estado, ejemplos Start, Done o WIP") //en el futuro que pueda elegir entre Ready to start / work in progress / done! 
+    if (status === null || status.trim() === "" || status === "" ){
+        alert("Debes ingresar un Estado.");
+        return
+    }
+    let deadline = prompt("ingresa la fecha de deadline como DD-MM-AA") // de momento lo escribe. Luego lo elige y Formato a mostrar en console: 3 de Enero a las 23:55 hs
+    if (deadline === null || deadline.trim() === "" || deadline === "" ){
+        alert("Debes ingresar una fecha límite.");
+        return
+    }
+    let frequency = prompt("ingresa cada cuanto tiempo se debe realizar la tarea") // de momento lo escribe.
+    if (frequency === null || frequency.trim() === "" || frequency === "" ){
+        alert("Debes ingresar una frecuencia.");
+        return
+    }
+    let newTask = new task (title,description,dateCreated,userAssigned,status,deadline,frequency) 
+    taskList.push(newTask)
+    tasksCount++;
+    console.table(taskList)
+    console.log("Numero total de tareas creadas hasta el momento: "+tasksCount)
+
+    if(tasksCount>2){
+        let askFilter = prompt("¿Quieres filtrar por Status? si / no ").toLowerCase();
+        if(askFilter === "si" || askFilter === "s"|| askFilter === "sí"){
+            statusFilter()
+        }
+    }
+}
+    
+    function statusFilter(){
+        let input = prompt("ingresá un status para filtrar como Start, Done o WIP").toLowerCase();
+        console.log("su input fue: " +input);
+        let filterResult = taskList.filter((task) => task.taskStatus.toLowerCase().includes(input));
+        console.log("filterResult es: " +filterResult);
+        if (filterResult.length > 0) {
+            console.log("entró en el IF de filterresultlenght >0");
+            let taskTitles = filterResult.map((task) => task.taskTitle).join(", ");
+            alert("las tareas que conciden con ese estado son "+ taskTitles);
+        }else{
+            alert("no se encontraron tareas con ese estado.")
+            }
+    }
+
+    const task = function(taskTitle,taskDescription,taskDateCreated,taskUserAssigned,taskStatus,taskDeadline,taskFrequency){
+        this.taskTitle = taskTitle   // ESTRUCTURA: this.(esto es lo que aparece luego en console como titulos de la tabla) = (esto debe coincidir con los parametros de la fc)
+        this.taskDescription = taskDescription
+        this.taskDateCreated = taskDateCreated //el sistema la toma en el momento que se crea
+        this.taskUserAssigned = taskUserAssigned
+        this.taskStatus = taskStatus //FUTURO desplegable: Ready to start / work in progress / done!
+        this.taskDeadline = taskDeadline // desplegable: format 3 de Enero a las 23:55 hs
+        this.taskFrequency = taskFrequency //desplegable que pueda elegir entre every day / every week / every XX days / every month / weekdays / weekends / etc
+    }
+    //let taskTest = new task("firstTask","Something to do","03-12-2024 23:55","user assigned","ready to start","January 3, 22:00 hs","every day") //solo para probarla
+    let taskList = []
+    
+    let answerCreate = true
+    do{
+        let askCreate = prompt("¿Quieres crear una tarea? pon `si´ o `no´. Si creas dos o más tareas completas podras filtrar").toLowerCase();
+        if(askCreate === "si" || askCreate === "s" || askCreate === "sí"){
+            answerCreate = true
+            newTask()
+        }
+        
+        else{
+            answerCreate = false;
+            alert("cuando quieras crear una tarea nueva, recarga el sitio y pon `si´ o `s´, por el momento funciona asi");
+            break
+            }
+        
+    }
+    while(answerCreate) //esto es lo mismo que poner while (answerCreate = true)
+           
+           
+  
